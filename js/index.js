e8a4053814f8a416ec08958526442007b4212456
Yulian <yulikdrummer@gmail.com>
2019-05-14 15:26:24 +0000

    init

--- js/index.js
+++ js/index.js
@@ -651,31 +651,8 @@ new TypeIt('#breakLines', {
 }).go();
 new TypeIt('.about-me', {
     strings: [],
-    speed: 30,
+    speed: 40,
     waitUntilVisible: true
 }).go();
 
 
-var linkNav = document.querySelectorAll('#home,#about-me,#contact-me,#my-projects'), //выбираем все ссылки к якорю на странице
-    V = 1;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
-for (var i = 0; i < linkNav.length; i++) {
-    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
-        e.preventDefault(); //отменяем стандартное поведение
-        var w = window.pageYOffset,  // производим прокрутка прокрутка
-            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
-        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
-            start = null;
-        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
-        function step(time) {
-            if (start === null) start = time;
-            var progress = time - start,
-                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-            window.scrollTo(0,r);
-            if (r != w + t) {
-                requestAnimationFrame(step)
-            } else {
-                location.hash = hash  // URL с хэшем
-            }
-        }
-    }, false);
-}
